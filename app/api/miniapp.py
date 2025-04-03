a27983f4534cf50b375d5a6420e0ba40caa28bec
BeeBeard <beebeard@yandex.ru>
2025-04-03 22:16:33 +0300

    Оставляем только тестовую страницу, base.css и base.js

--- app/api/miniapp.py
+++ app/api/miniapp.py
@@ -16,82 +16,14 @@ r_miniapp = APIRouter(prefix=CONFIG.miniapp.root, tags=['FRONTEND'])
 r_invitation = APIRouter(tags=['INVITATION'])
 
 
-@r_miniapp.get(f"/t1", response_class=HTMLResponse)
+@r_miniapp.get(f"/test_page", response_class=HTMLResponse)
 async def open_main_menu(request: Request):
-    """
-    Для проверки соединения
-    Стартовая страница app
-    """
+    """Для проверки соединения стартовая страница miniapp"""
+
     pprint.pprint(request)
     return templates.TemplateResponse(
-        "pages/main_menu.html",
-        {"request": request, 'active_tab': 'main_menu'}
-    )
-
-
-@r_miniapp.get(f"/t2", response_class=HTMLResponse)
-async def open_create_event(request: Request, context: MiniappEvent = None):
-
-    context = MiniappEvent.parse_obj(context) if context else MiniappEvent()
-    context = context.model_dump()
-
-    return templates.TemplateResponse(
-        "pages/create_event.html",
-        {"request": request, "context": context}
-    )
-
-
-# noinspection PyProtectedMember,PyUnresolvedReferences
-@r_miniapp.get(f"/t3", response_class=HTMLResponse)
-async def open_edit_event(event_id: str, request: Request, preload_context: MiniappEvent = None):
-
-    # if not preload_context:
-    #     result = await sql.get_event(event_id=event_id)
-    #     # noinspection PyUnresolvedReferences
-    #     result_dict = result._asdict()
-    #     pprint.pprint(f"Полученные данные из базы\n\n{result_dict}")
-    #     context = MiniappEvent.parse_obj(result_dict)
-    #     pprint.pprint(f"Схема \n\n{context}")
-    #
-    # else:
-
-    context = MiniappEvent.parse_obj(preload_context)
-
-    context.manage_type = "edit_event"
-
-    return templates.TemplateResponse(
-        "pages/create_event.html",
-        {
-            "request": request,
-            "context": context
-        }
-    )
-
-
-# Отдельный endpoint для короткой ссылки
-# noinspection PyUnresolvedReferences,PyProtectedMember
-@r_invitation.get("/{event_id}", response_class=HTMLResponse)
-async def open_add_attendees(event_id: str, request: Request, preload_context: MiniappEvent = None):
-
-    # if not preload_context:
-    #     result = await sql.get_event(event_id=event_id)
-    #     # noinspection PyUnresolvedReferences,PyProtectedMember
-    #     result_dict = result._asdict()
-    #     pprint.pprint(f"Полученные данные из базы\n\n{result_dict}")
-    #     context = MiniappEvent.parse_obj(result_dict)
-    #     pprint.pprint(f"Схема \n\n{context}")
-    #
-    # else:
-    context = MiniappEvent.parse_obj(preload_context)
-
-    context.manage_type = "edit_event"
-
-    return templates.TemplateResponse(
-        "pages/add_attendees.html",
-        {
-            "request": request,
-            "context": context
-        }
+        "pages/test_page.html",
+        {"request": request, "root": CONFIG.project.root}
     )
 
 
