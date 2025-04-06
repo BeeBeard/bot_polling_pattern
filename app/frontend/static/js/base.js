54baa09e0bf26b4090edd4d8ff584a4b2ca05b09
BeeBeard <beebeard@yandex.ru>
2025-04-06 14:05:38 +0300

    Тест выключения приложения если открыт не через TG

--- app/frontend/static/js/base.js
+++ app/frontend/static/js/base.js
@@ -1,5 +1,5 @@
 function add_listener() {
-    document.getElementById('testButton').addEventListener('onclick', function(event) {
+    document.getElementById('testButton').addEventListener('click', function(event) {
         alert("Вы нажали на кнопку, скрипты отрабатывают")
         tg.close()
     }
